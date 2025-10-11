3aad49dd324b0dcd55ef6b341df187efd5d7e932
vinaydeaws <vinaydeaws@gmail.com>
2025-10-12 00:12:35 +0530

    correction in variables.tf

--- vpc_module/variables.tf
+++ vpc_module/variables.tf
@@ -11,7 +11,7 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets_cidr" {
-  description = "List of CIDR blocks for the public subnets."
+  description = "A list of CIDR blocks for the public subnets."
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
