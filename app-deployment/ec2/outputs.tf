bc507c73c4d2789fe3e057010ca20a670995829b
vinay <vinaydeaws@gmail.com>
2025-10-25 04:29:09 +0530

    .

--- app-deployment/ec2/outputs.tf
+++ app-deployment/ec2/outputs.tf
@@ -8,18 +8,36 @@ output "alb_dns_name" {
   value       = data.terraform_remote_state.ALB.outputs.alb_dns_name
 }
 
+# Output the bastion public IP
 output "bastion_public_ip" {
-  description = "The public IP of the Bastion Host."
+  description = "The public IP address of the Bastion Host."
+  # Reference the output exposed by the 'ec2' module
   value       = module.ec2.bastion_public_ip
 }
 
-output "app_server_private_ips" {
-  description = "Private IPs of the application servers."
-  value       = module.ec2.private_ip_addresses
+# Output the private IP of the Master server
+output "master_private_ip" {
+  description = "The private IP address of the Master EC2 instance."
+  # Access the first element (Master) of the 'private_ips' list exposed by the module
+  value       = element(module.ec2.private_ips, 0)
 }
 
-output "ssh_private_key" {
-  description = "The generated private key for SSH login."
-  value       = module.ec2.ssh_private_key
-  sensitive   = true
+# Output the private IP of Node-1 server
+output "node_1_private_ip" {
+  description = "The private IP address of Node-1 EC2 instance."
+  # Access the second element (Node-1) of the 'private_ips' list
+  value       = element(module.ec2.private_ips, 1)
+}
+
+# Output the private IP of Node-2 server
+output "node_2_private_ip" {
+  description = "The private IP address of Node-2 EC2 instance."
+  # Access the third element (Node-2) of the 'private_ips' list
+  value       = element(module.ec2.private_ips, 2)
+}
+
+# Output the key name
+output "key_pair_name" {
+  description = "The name of the SSH key pair."
+  value       = module.ec2.key_pair_name
 }
--- modules/ec2_module/output.tf
+++ modules/ec2_module/output.tf
@@ -10,26 +10,14 @@ output "key_file_path" {
   value       = local_file.key_pem.filename
 }
 
-# Output the public IP of the Bastion Host
+# Output the private IPs as a list
+output "private_ips" {
+  description = "List of private IPs for Master, Node-1, and Node-2."
+  value       = aws_instance.private_servers.*.private_ip
+}
+
+# Output the bastion public IP
 output "bastion_public_ip" {
   description = "The public IP address of the Bastion Host."
   value       = aws_eip.bastion_eip.public_ip
-}
-
-# Output the private IP of the Master server
-output "master_private_ip" {
-  description = "The private IP address of the Master EC2 instance (index 0)."
-  value       = aws_instance.private_servers[0].private_ip
-}
-
-# Output the private IP of Node-1 server
-output "node_1_private_ip" {
-  description = "The private IP address of Node-1 EC2 instance (index 1)."
-  value       = aws_instance.private_servers[1].private_ip
-}
-
-# Output the private IP of Node-2 server
-output "node_2_private_ip" {
-  description = "The private IP address of Node-2 EC2 instance (index 2)."
-  value       = aws_instance.private_servers[2].private_ip
 }
